f9ba5f1f7835013fa6ab808320c2eb19ef08fa40
Anthony <martab7.am@gmail.com>
2016-08-30 15:51:09 -0500

    Creates CRUD for all edited controllers.

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
 
 class EventsController extends Controller
 {
@@ -16,7 +18,7 @@ class EventsController extends Controller
      */
     public function index()
     {
-        // $events = 
+        // $events =
         return view('events.all');
     }
 
@@ -27,7 +29,7 @@ class EventsController extends Controller
      */
     public function create()
     {
-        //
+        return view('events.create');
     }
 
     /**
@@ -38,7 +40,8 @@ class EventsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $event = new Event();
+      return $this->validateAndSave($event, $request);
     }
 
     /**
@@ -49,7 +52,10 @@ class EventsController extends Controller
      */
     public function show($id)
     {
-        //
+      $event = Event::findOrFail($id);
+      $company = $event->companies;
+      $data = compact('event', 'company');
+      return view('')->with($data);
     }
 
     /**
@@ -60,7 +66,9 @@ class EventsController extends Controller
      */
     public function edit($id)
     {
-        //
+      $event = Event::findOrFail($id);
+      $data = compact('event');
+      return view('companies.edit_account_company')->with($data);
     }
 
     /**
@@ -72,7 +80,8 @@ class EventsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $event = Event::findOrFail($id);
+      return $this->validateAndSave($event, $request);
     }
 
     /**
@@ -83,6 +92,31 @@ class EventsController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $event = Event::findOrFail($id);
+      $event->delete();
+      return redirect()->action('companies.view_profile');
     }
+
+    private function validateAndSave(Event $event, Request $request){
+        $company_id = Auth::user()->id;
+        $is_admin = Auth::user()->is_admin;
+        $request->session()->flash('ERROR_MESSAGE', 'User was not created successfully'); //set error message if not saved
+        $this->validate($request, Event::$rules); //validate that alll fields are filled out correctly
+        $request->session()->forget('ERROR_MESSAGE'); // if validated, tell to forget the error message
+        $event->company_id = $company_id;
+        $event->desc = $request->desc;
+        $event->from_date = $request->from_date;
+        $event->to_date = $request->to_date;
+        $event->invite_only = $request->invite_only;
+        $event->rsvp_required = $request->rsvp_required;
+        $event->url = $request->url;
+        $event->save(); //save when submited
+        // Log::info('User successfully creates post', $request->all()); // create custom log when post is created
+        if($is_admin){
+          $request->session()->flash('message', 'Event was successful!'); // flash success message when saved
+          return redirect()->action('admin.dashboard'); //redirect to the index page
+        } else {
+          $request->session()->flash('message', 'Event was successfully created!'); // flash success message when saved
+          return redirect()->action('companies.view_profile');
+        }
 }
