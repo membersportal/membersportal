cca94ae0c867319d4dc3075e39580e4cb2fc79c7
Jay Nichols <jaynichols@Jays-MacBook-Air-3.local>
2016-08-30 11:10:07 -0500

    getters, setters, roles

--- app/Carousel.php
+++ app/Carousel.php
@@ -5,6 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Carousel extends Model
-{
+{	
+	public static $rules = [
+     'title' => 'required|max:75',
+     'desc' => 'required|filled',
+     'img' => 'required|image',
+     'url' => 'required|url'
+   	];
     //
 }
+
--- app/Leader.php
+++ app/Leader.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Company;
 
 class Leader extends Model
 {
+	use SoftDeletes;
+
+	public function companies()
+	{
+		return $this->belongsTo(Company::class, 'company_id');
+	}
+
+	public static $rules = [
+     'full_name' => 'required|max:100',
+     'title' => 'required|max:100',
+     'img' => 'required|image',
+     'linkedin_url' => 'required|url'
+   	];
     //
 }
