d43c910dc08634b2302f7c9a36458db148d9afa6
Randi Mays <randimays1024@gmail.com>
2016-09-01 11:24:48 -0500

    Created search filter section for search page.

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 	<h1 class="text-center">Search Members</h1>
 	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+		<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xl-offset-2">
 			<div id="search_filter">
-				<form method="GET" action=" {{ action('CompaniesController@searchMembers') }}">
+				<form method="GET" action="{{ action('CompaniesController@searchMembers') }}">
 					{!! csrf_field() !!}
 					<div class="form-group">
 		  				<input type="text" name="searchField" class="form-control" placeholder="Company name, description or keywords">
@@ -14,28 +14,29 @@
 					<select class="form-control" name="industry_id" required>
 						<option value="0">Select</option>
 						@foreach ($industries as $industry)
-							<option value="{{ $industry->id }}" {{ $company->industry_id == '$industry->id' ? 'selected' : '' }}>{{ $industry }}</option>
+							<option value="{{ $industry->id }}">{{ $industry->industry }}</option>
+						@endforeach
 					</select>
-				<div class="checkbox-inline">
-					<label for="contractor">
-					<input type="checkbox" name="contractor" value="contractor"> Contractor
-					</label>
-				</div>
-				<div class="checkbox-inline">
-					<label for="organization">
-					<input type="checkbox" name="organization" value="organization"> Organization
-					</label>
-				</div>
-				<div class="checkbox-inline">
-					<label for="contractor">
-					<input type="checkbox" name="family_owned" value="family_owned"> Family-Owned
-					</label>
-				</div>
-				<div class="checkbox-inline">
-					<label for="woman_owned">
-					<input type="checkbox" name="woman_owned" value="woman_owned"> Woman-Owned
-					</label>
-				</div>
+					<div class="checkbox-inline">
+						<label for="contractor">
+						<input type="checkbox" name="contractor" value="contractor"> Contractor
+						</label>
+					</div>
+					<div class="checkbox-inline">
+						<label for="organization">
+						<input type="checkbox" name="organization" value="organization"> Organization
+						</label>
+					</div>
+					<div class="checkbox-inline">
+						<label for="contractor">
+						<input type="checkbox" name="family_owned" value="family_owned"> Family-Owned
+						</label>
+					</div>
+					<div class="checkbox-inline">
+						<label for="woman_owned">
+						<input type="checkbox" name="woman_owned" value="woman_owned"> Woman-Owned
+						</label>
+					</div>
 					<button type="submit" class="btn btn-default">Submit</button>
 	  			</form>
 			</div>
