d33944e1293d870ecebc7588acfbc2024719df66
Jay Nichols <jaynichols@Jays-MacBook-Air-3.local>
2016-09-01 16:40:04 -0500

    updated twitter function

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,7 +101,8 @@
 
 	<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 right_home">
 		<h3 class="text-center">Events</h3>
-		 <a class="twitter-timeline"  href="https://twitter.com/search?q=from%3Ajmills_business" data-widget-id="771057747718582272" data-screen-name="jmills_business">Tweets about </a>
+		 dd($admin_user)
+		 <a class="twitter-timeline"  href="https://twitter.com/search?q=from%3A{{ $admin_user->contact()->twitter }}" data-widget-id="771057747718582272" data-screen-name="{{ $admin_user->contact()->twitter }}">Tweets about </a>
          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	</div>
 </div>
