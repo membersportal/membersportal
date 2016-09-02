b0722979b3d3a0a626003ffa48fe559fd4fb9eb5
Jay Nichols <jaynichols@Jays-MacBook-Air-3.local>
2016-09-02 17:11:09 -0500

    added login and connect

--- resources/views/admin/create_account_login.blade.php
+++ resources/views/admin/create_account_login.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <form method="POST" action="{{ action('AuthController@store') }}">
+	{!!csrf_field()!!}
 @include(admin_create_login.blade.php)
 <button type="submit">Submit</button>
 </form>
