cff56ed29aa1e84d708a3633408ce8f8c9dbe0ac
Anthony <martab7.am@gmail.com>
2016-09-03 10:39:55 -0500

    Preparing for merge

--- resources/views/companies/edit_account_company.blade.php
+++ resources/views/companies/edit_account_company.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <div class="row">
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 left_home">
-
+    <ul>
+      <li>Manage Login</li>
+      <li>Manage Company Information</li>
+      <li>Manage Contact Information</li>
+    </ul>
   </div>
 
   <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 center_home">
