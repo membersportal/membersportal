6d844865d5c02b1476728c0445102b15fc2e46ff
Anthony <martab7.am@gmail.com>
2016-09-04 16:28:04 -0500

    Fixing dashboard views

--- resources/views/companies/companies_dashboard.blade.php
+++ resources/views/companies/companies_dashboard.blade.php
@@ -19,7 +19,15 @@
 
 
   <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 center_home">
-
+    @foreach($feed_content as $content)
+      @if($content->company1_id)
+        <h1>{{ $content->company1_id }}</h1>
+      @elseif($content->project_title)
+        <h2>{{ $content->project_title }}
+      @elseif($content->title)
+        <h3>{{ $content->title }}</h3>
+      @endif
+    @endforeach
 
   </div>
 
