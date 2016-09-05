cfc769ba361b6508f14e46191bad24075d53833a
Randi Mays <randimays1024@gmail.com>
2016-09-05 12:20:39 -0500

    Updating config files

--- app/User.php
+++ app/User.php
@@ -10,7 +10,6 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
 use App\Company;
 use App\Contact;
 use App\Connection;
