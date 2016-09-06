325e85fcb3bd79c4966af3a8206ac61fe781a8f1
Randi Mays <randimays1024@gmail.com>
2016-09-06 15:36:09 -0500

    Tweaking one thing with the jQuery on company edit page.

--- resources/views/companies/edit_company.blade.php
+++ resources/views/companies/edit_company.blade.php
@@ -5,7 +5,6 @@
 <div class="container">
 	<h1 class="text-center">Edit Company Information</h1>
 	<p class="text-center"><span class="required">*</span> Required Field</p>
-
 	<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 edit_nav">
 		<div class="row edit_account_nav">
 			<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 col-xl-11">
@@ -18,20 +17,16 @@
 			</div>
 		</div>
 	</div>
-
-		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 center_home">
-
-			<form enctype="multipart/form-data" action="{{ action('CompaniesController@update', ['id' => $company->id]) }}" method="POST">
-				{!! csrf_field() !!}
-				@include('partials.edit_company_form')
-				@include('partials.edit_company_images_form')
-				<button class="btn btn-primary pull-right" type="submit">Save</button>
-				<a class="cancel_button pull-right" href="{{ action('UsersController@index') }}" alt="cancel">Cancel</a>
-			</form>
-		</div>
-
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 center_home">
+		<form enctype="multipart/form-data" action="{{ action('CompaniesController@update', ['id' => $company->id]) }}" method="POST">
+			{!! csrf_field() !!}
+			@include('partials.edit_company_form')
+			@include('partials.edit_company_images_form')
+			<button class="btn btn-primary pull-right" type="submit">Save</button>
+			<a class="cancel_button pull-right" href="{{ action('UsersController@index') }}" alt="cancel">Cancel</a>
+		</form>
+	</div>
 </div>
-
 @stop
 
 @section('bottom-script')
@@ -42,6 +37,7 @@
 			var business_type = $('#business_type').val();
 			if (business_type == 'contractor') {
 				$('#size').attr('disabled', true);
+				$('#size').val() = null;
 			} else {
 				$('#size').attr('disabled', false);
 			}
