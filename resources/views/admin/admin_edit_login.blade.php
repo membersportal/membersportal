a4d22f6ee14b5e5d5cc6cde178cafa5aca74d65f
Randi Mays <randimays1024@gmail.com>
2016-09-07 14:17:30 -0500

    Making changes to the admin dashboard for styling and routing

--- resources/views/admin/admin_edit_login.blade.php
+++ resources/views/admin/admin_edit_login.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
 <div class="container">
+	<h1 class="text-center">Edit Login Information</h1>
 	<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1 edit_nav">	
 		<ul class="edit_account_nav">
-			<li><a class="active" href="{{ action('UsersController@getAdminDashboard') }}" alt="Analytics">Analytics</a></li>
-			<li><a href="{{ action('UsersController@edit', ['id' => Auth::user()->id]) }}" alt="Edit Admin Login">Edit My Login</a></li>
+			<li><a href="{{ action('UsersController@getAdminDashboard') }}" alt="Analytics">Analytics</a></li>
+			<li><a class="active" href="{{ action('UsersController@edit', ['id' => Auth::user()->id]) }}" alt="Edit Admin Login">Edit My Login</a></li>
 			<li><a href="{{ action('ContactsController@edit', ['id' => Auth::user()->id]) }}" alt="Edit Org Contact">Edit Org. Contact</a></li>
 			<li><a href="{{ action('ArticlesController@adminIndex') }}" alt="Manage Articles">Articles</a></li>
 			<li><a href="{{ action('CarouselsController@adminIndex') }}" alt="Manage Carousels">Carousels</a></li>
