7a38efa60be88698330d8ad4d825c85cf9fe2fcc
Anthony <martab7.am@gmail.com>
2016-09-07 15:11:39 -0500

    Preparing for merge

--- resources/views/companies/companies_dashboard.blade.php
+++ resources/views/companies/companies_dashboard.blade.php
@@ -4,7 +4,31 @@
 <div class="row">
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 left_home">
 
-
+    <div class="panel_white">
+      <h3 class="text-center">My RFPs</h3>
+      @foreach ($users_rfps as $key => $rfp)
+        @if ($key < 3)
+        <div id="accordion" role="tablist" aria-multiselectable="false">
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading{{$key+1}}">
+              <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key+1}}" aria-expanded="false" aria-controls="collapse{{$key+1}}">{{ $rfp->project_title }}
+              </a>
+              </h4>
+              <p class="event_date_home">Deadline for submission: {{ $rfp->deadline }} }}</p>
+            </div>
+            <div id="collapse{{$key+1}}" class="panel-collapse collapse event_desc_home" role="tabpanel" aria-labelledby="heading{{$key+1}}">
+            {{ str_limit($rfp->project_scope, 100) }}<a class="red_link" href="{{ action('RFPController@show', $id = $rfp->id) }}"> see request</a>
+            </div>
+          </div>
+        </div>
+        @endif
+      @endforeach
+      <a href="{{ action('RFPController@create') }}">Create New RFP</a>
+      <div class="panel_green">
+        <a class="green_bg" href="{{ action('RFPController@index') }}" alt="View All RFPs">See All RFPs</a>
+      </div>
+    </div>
 
   </div>
 
@@ -24,8 +48,31 @@
 
 
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 right_home">
+    <div class="panel_white">
+			<h3 class="text-center">My Events</h3>
+			@foreach ($users_events as $key => $event)
+				@if ($key < 3)
+				<div id="accordion" role="tablist" aria-multiselectable="false">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading{{$key+1}}">
+							<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key+1}}" aria-expanded="false" aria-controls="collapse{{$key+1}}">{{ $event->title }}
+							</a>
+							</h4>
+							<p class="event_date_home">{{ $event->from_date->format('F j') }} - {{ $event->to_date->format('F j') }}</p>
+						</div>
+						<div id="collapse{{$key+1}}" class="panel-collapse collapse event_desc_home" role="tabpanel" aria-labelledby="heading{{$key+1}}">
+						{{ str_limit($event->desc, 100) }}<a class="red_link" href="{{ action('EventsController@show', $id = $event->id) }}"> see event</a>
+						</div>
+					</div>
+				</div>
+				@endif
+			@endforeach
+			<div class="panel_green">
+				<a class="green_bg" href="{{ action('EventsController@index') }}" alt="View All Events">See All Events</a>
+			</div>
+		</div>
 
-    
 
   </div>
 </div>
--- resources/views/events/edit_event.blade.php
+++ resources/views/events/edit_event.blade.php
@@ -35,17 +35,30 @@
 
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 right_home">
 
-    <form class="" action="{{ action('EventsController@edit', ['id' => 'event-id']) }}" method="GET">
-      <label for="users_events">My Events</label>
-      <select class="form-control" id="event_id" name="event_id">
-        <option disabled selected label="Select"></option>
-        @foreach ($users_events as $event)
-          <option value="{{ $event->id }}">{{ $event->title }}</option>
-        @endforeach
-      </select>
-      <a href="{{ action('EventsController@create') }}">Add new event</a>
-      <button type="submit" class="btn btn-default">Edit</button>
-    </form>
+    <div class="panel_white">
+			<h3 class="text-center">My Events</h3>
+			@foreach ($users_events as $key => $event)
+				@if ($key < 3)
+				<div id="accordion" role="tablist" aria-multiselectable="false">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading{{$key+1}}">
+							<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key+1}}" aria-expanded="false" aria-controls="collapse{{$key+1}}">{{ $event->title }}
+							</a>
+							</h4>
+							<p class="event_date_home">{{ $event->from_date->format('F j') }} - {{ $event->to_date->format('F j') }}</p>
+						</div>
+						<div id="collapse{{$key+1}}" class="panel-collapse collapse event_desc_home" role="tabpanel" aria-labelledby="heading{{$key+1}}">
+						{{ str_limit($event->desc, 100) }}<a class="red_link" href="{{ action('EventsController@show', $id = $event->id) }}"> see event</a>
+						</div>
+					</div>
+				</div>
+				@endif
+			@endforeach
+			<div class="panel_green">
+				<a class="green_bg" href="{{ action('EventsController@index') }}" alt="View All Events">See All Events</a>
+			</div>
+		</div>
 
 
   <div class="panel_white">
