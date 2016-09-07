af36ac5866dfb1e162af99721366cbcb1d60e2cf
Anthony <martab7.am@gmail.com>
2016-09-07 14:17:31 -0500

    Sublte changes for now

--- resources/views/companies/companies_dashboard.blade.php
+++ resources/views/companies/companies_dashboard.blade.php
@@ -4,16 +4,7 @@
 <div class="row">
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 left_home">
 
-    <form class="form-control" action="{{ action('RFPController@edit') }}" method="get">
-      <label for="users_rfps">My RFPs</label>
-      <select class="form-control" id="rfp_id" name="rfp_id">
-        <option disabled selected label="Select"></option>
-        @foreach ($users_rfps as $rfp)
-          <option value="{{ $rfp->id }}">{{ $rfp->project_title }}</option>
-        @endforeach
-      </select>
-      <button type="submit" class="btn btn-default">Edit</button>
-    </form>
+
 
   </div>
 
@@ -34,16 +25,7 @@
 
   <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 right_home">
 
-    <form class="" action="{{ action('EventsController@edit') }}" method="GET">
-      <label for="users_events">My Events</label>
-      <select class="form-control" id="event_id" name="event_id">
-        <option disabled selected label="Select"></option>
-        @foreach ($users_events as $event)
-          <option value="{{ $event->id }}">{{ $event->title }}</option>
-        @endforeach
-      </select>
-      <button type="submit" class="btn btn-default">Edit</button>
-    </form>
+    
 
   </div>
 </div>
