3dcfea06adb4897d06cccf63e42c2d583a2ac090
Randi Mays <randimays1024@gmail.com>
2016-09-07 22:49:53 -0500

    Adding a create form to the articles section in admin dashboard

--- resources/views/admin/admin_create_article.blade.php
+++ resources/views/admin/admin_create_article.blade.php
@@ -2,9 +2,22 @@
 
 @section ('content')
 
-<form method="POST" action="{{ action('ArticlesController@store') }}">
-	{!!csrf_field()!!}
-	@include ('partials.admin_create_article')
-<button type="submit">Create</button>
-</form>
+<div class="container">
+	<h1 class="text-center">Create Article</h1>
+	<p class="text-center"><span class="required">*</span> Required Field</p>
+		<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1 edit_nav">	
+			@include('partials.admin_dash_nav', ['home' => '', 'login' => '', 'contact' => '', 'articles' => 'active', 'carousels' => '', 'events' => '', 'rfps' => '', 'users' => ''] )
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+			<div class="panel_form">
+				<form method="POST" action="{{ action('ArticlesController@store') }}">
+					{!!csrf_field()!!}
+					@include ('partials.admin_create_article_form')
+					<button class="btn btn-primary pull-right" type="submit">Create</button>
+				</form>
+				<a class="cancel_button pull-right" href="{{ action('ArticlesController@adminIndex') }}" alt="cancel">Cancel</a>
+			</div>
+		</div>
+</div>
+
 @stop
