30f51d68f75e22b736b26390599a76d32fadf225
Anthony <martab7.am@gmail.com>
2016-09-08 12:54:16 -0500

    Leaders capabilities checked

--- resources/views/leaders/edit_leader.blade.php
+++ resources/views/leaders/edit_leader.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 <div class="container">
-	<h1 class="text-center">Edit Login Information</h1>
+	<h1 class="text-center">Edit Leaders</h1>
 	<p class="text-center"><span class="required">*</span> Required Field</p>
 	<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1 edit_nav">
-		@include('partials.edit_account_nav', ['login' => 'active', 'company' => '', 'contact' => '', 'leaders' => ''])
+		@include('partials.edit_account_nav', ['login' => '', 'company' => '', 'contact' => '', 'leaders' => 'active'])
 	</div>
 	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
 		<div class="panel_form">
-			<form action="POST" action="{{ action('UsersController@update', ['id' => Auth::user()->id]) }}">
+			<form method="POST" action="{{ action('LeadersController@update', ['id' => $leader->id]) }}" enctype="multipart/form-data">
 				{!! csrf_field() !!}
-				@include('partials.edit_login_form')
+        {{ method_field('PUT') }}
+				@include('partials.edit_leader_form')
 				<button class="btn btn-primary pull-right" type="submit">Save</button>
-				<a class="cancel_button pull-right" href="{{ action('UsersController@index') }}" alt="cancel">Cancel</a>
+				<a class="cancel_button pull-right" href="{{ action('LeadersController@index') }}" alt="cancel">Cancel</a>
 			</form>
 		</div>
 	</div>
