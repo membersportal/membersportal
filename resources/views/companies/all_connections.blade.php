74609a2ddbf6340c533f96ab8da96b86000d4089
Randi Mays <randimays1024@gmail.com>
2016-09-08 17:33:03 -0500

    Styling connections page

--- resources/views/companies/all_connections.blade.php
+++ resources/views/companies/all_connections.blade.php
@@ -3,46 +3,48 @@
 @section('content')
 
 <div class="container">
-	<h1 class="text-center">My Connections</h1>
+	<h1 class="text-center space">My Connections</h1>
 		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 left_home">
 			<div class="panel_white">
-				<h3 class="text-center">My RFPs</h3>
-				@foreach ($users_rfps as $key => $rfp)
-					@if ($key < 3)
-					<div id="accordion" role="tablist" aria-multiselectable="false">
-						<div class="panel panel-default">
-							<div class="panel-heading" role="tab" id="heading{{$key+1}}">
-								<h4 class="panel-title">
-								<a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key+1}}" aria-expanded="false" aria-controls="collapse{{$key+1}}">{{ $rfp->project_title }}
-								</a>
-								</h4>
-								<p class="event_date_home">Deadline: {{ $rfp->deadline }}</p>
-							</div>
-							<div id="collapse{{$key+1}}" class="panel-collapse collapse event_desc_home" role="tabpanel" aria-labelledby="heading{{$key+1}}">
-							{{ str_limit($rfp->project_scope, 100) }}<a class="red_link" href="{{ action('RFPController@show', $id = $rfp->id) }}"> see request</a>
-							</div>
+			<h3 class="text-center">Connections' Events</h3>
+			@foreach ($connections_events as $key => $event)
+				@if ($key < 3)
+				<div id="accordion" role="tablist" aria-multiselectable="false">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading{{$key+1}}">
+							<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$key+1}}" aria-expanded="false" aria-controls="collapse{{$key+1}}">{{ $event->title }}
+							</a>
+							</h4>
+							<p class="event_date_home">{{ $event->from_date->format('F j') }} - {{ $event->to_date->format('F j') }}</p>
+						</div>
+						<div id="collapse{{$key+1}}" class="panel-collapse collapse event_desc_home" role="tabpanel" aria-labelledby="heading{{$key+1}}">
+						{{ str_limit($event->desc, 100) }}<a class="red_link" href="{{ $event->url }}" target="_blank"> see event</a>
 						</div>
 					</div>
-					@endif
-				@endforeach
-				<div class="panel_green">
-					<a class="green_bg" href="{{ action('RFPController@index') }}" alt="View All RFPs">See All RFPs</a>
-				</div>
-				@if(!Auth::user()->is_admin)
-				<div class="panel_beige">
-					<a class="beige_bg" href="{{ action('RFPController@create') }}" alt="Create New RFP">Create New RFP</a>
 				</div>
 				@endif
+			@endforeach
+			<div class="panel_green">
+				<a class="green_bg" href="{{ action('EventsController@index') }}" alt="View All Events">See All Events</a>
 			</div>
-
-	</div>
-
-
-	@foreach($connections as $connection)
-		<img src="{{ '/img/uploads/avatars/' . $connection->profile_img }} " alt="" />
-		<a href="{{ action('CompaniesController@show', $id = $connection->id) }}"> {{ $connection->name }}</a>
-	@endforeach
-
+		</div>
+		</div>
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+			<div class="row">
+			@foreach ($connections as $connection)
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+					<div class="panel_white connections_grid">
+						<a href="{{ action('CompaniesController@show', ['id' => $connection->id]) }}" alt="{{ $connection->name }} Profile">
+							<img class="img-circle img-thumbnail center-block img-responsive connections_grid" src="{{ '/img/uploads/avatars/' . $connection->profile_img }}">
+						</a>
+						<p class="company_name">{{ $connection->name }}</p>
+						<p class="industry_name">{{ $connection->industry->industry }}</p>
+					</div>
+				</div>
+			@endforeach
+			</div>
+		</div>
 </div>
 
 @stop
