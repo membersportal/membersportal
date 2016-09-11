80241b34b9c697061b58b813e68c095652a265b6
Anthony <martab7.am@gmail.com>
2016-09-11 12:06:51 -0500

    Updates view all pages for events and rfps to show individual instance when clicked

--- resources/views/rfps/all_rfps.blade.php
+++ resources/views/rfps/all_rfps.blade.php
@@ -28,7 +28,7 @@
 		@foreach ($rfps as $key => $rfp)
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-					<h4 class="rfp_heading"><a href="{{ $rfp->url ? $rfp->url : '#' }}" target="_blank">{{ $rfp->project_title }}</a></h4>
+					<h4 class="rfp_heading"><a href="{{ action('RFPController@show', $id = $rfp->id) }}">{{ $rfp->project_title }}</a></h4>
 					<p class="rfp_dates">Deadline: <span class="rfp_date">{{ $rfp->deadline }}</span></p>&nbsp;&nbsp; || &nbsp;&nbsp;
 					<p class="rfp_dates">Project Dates: <span class="rfp_date">{{ $rfp->contract_from_date }} - {{ $rfp->contract_to_date }}</span></p>
 					<p class="rfp_contact">Contact: <span class="rfp_contact_detail">{{ $rfp->contact_name }}</span>&nbsp;&nbsp; || &nbsp;&nbsp;Dept: <span class="rfp_contact_detail">{{ $rfp->contact_department }}</span>&nbsp;&nbsp; || &nbsp;&nbsp;Phone:</span> <span class="rfp_contact_detail">{{ $rfp->contact_no }}</span></p>
