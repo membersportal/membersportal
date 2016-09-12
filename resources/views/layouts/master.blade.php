f77fa19d15565a7a54d6d65cab5706f92bf94b43
Anthony <martab7.am@gmail.com>
2016-09-12 16:09:11 -0500

    Merging soon and adding google analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,16 @@
 	<link href="/css/site.css" rel="stylesheet">
 	<link href="/css/navbar.css" rel="stylesheet">
 	<link href="/css/carousel.css" rel="stylesheet">
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-84061205-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 	@if (Auth::check())
